6fa01d113ea9aece746d405786ce3a227865d824
saranjeet singh <grewalele@gmail.com>
2020-07-05 18:50:14 -0400

    function-3

--- style.js
+++ style.js
@@ -1,12 +1,17 @@
-function getSize(width, height, depth) {
-    var area = width*height;
-    var volume = width*height*depth;
-    var sizes = [area , volume]
-    return sizes
+function Hotel(name, rooms, booked) {
+    this.name = name;
+    this.rooms = rooms;
+    this.booked = booked;
+
+    this.checkAvailability = function(){
+        return this.rooms - this.booked
+    }
 }
 
-var areaOne = getSize(3,2,3) [0];
-var volumeOne =  getSize(3,2,3) [1];
+var hotel1 = new Hotel('king' , 33, 3)
 
-console.log(areaOne)
-console.log(volumeOne)
+var elName = document.getElementById('hotelName');
+elName.textContent = 'Hotel Name: '+ hotel1.name
+
+var elRooms = document.getElementById('rooms');
+elRooms.textContent = 'No of rooms vacant: ' + hotel1.checkAvailability();
