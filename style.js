eb1cd5d383552d4294fa2e97447a0740d893ee15
saranjeet singh <grewalele@gmail.com>
2020-07-08 16:59:44 -0400

    delete extra

--- style.js
+++ style.js
@@ -1,14 +1,1 @@
-const sq1 = x => x*x;
-console.log(sq1(4))
-
-const sq2 = function(x) {
-    return x*x;
-}
-
-console.log(sq2(3))
-
-const horn = () => {
-    console.log("Toot");
-    };
-horn()
 
