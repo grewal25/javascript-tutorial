a7baa0582a1c4a251e4e2386db93695080ba9dd1
saranjeet singh <grewalele@gmail.com>
2020-07-09 11:09:59 -0400

    code add

--- style.js
+++ style.js
@@ -1,1 +1,20 @@
 
+const select = document.querySelector('select');
+const html = document.querySelector('html')
+console.log(html)
+
+function update(bgColor, textColor){
+    html.style.backgroundColor = bgColor;
+    html.style.color = textColor;
+
+}
+
+function changeColor(){
+    if (select.value === 'black'){
+        update('black', 'white')
+    }else {
+        update('white', 'black');
+    }
+}
+
+select.onchange = changeColor;
