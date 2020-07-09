adb62057187482d77f2aa2826386a6c101b24a95
saranjeet singh <grewalele@gmail.com>
2020-07-09 08:08:47 -0400

    add

--- style.js
+++ style.js
@@ -1,1 +1,20 @@
+const select = document.querySelector('select');
+const para = document.querySelector('h1')
 
+select.addEventListener('change', setWeather);
+
+function setWeather(){
+    const choice = select.value;
+
+    if (choice === 'sunny'){
+        para.textContent = 'nice sunny day';
+    } else if (choice === 'rainy'){
+        para.textContent =' rainy day';
+    }else if (choice === 'snowing'){
+        para.textContent ='it will snow';
+    }else if (choice === 'overcast'){
+        para.textContent ='cloud';
+    } else {
+        para.textContent = 'i dont know what you ask' ;
+    }
+}
